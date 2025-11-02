692c990261d7e06092092faef21031445ba41a17
oliverwunder20-sys <oliverwunder20@gmail.com>
2025-11-02 19:14:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE = 'aescher-v2';
+const CACHE = 'aescher-planer-v29';
 const ASSETS = [
-  './','./index.html','./styles.css','./app.js',
-  './manifest.json','./assets/icon-192.png','./assets/icon-512.png','./assets/suedleder-icons.svg'
+  './','./index.html','./styles.css','./app.js','./manifest.json',
+  './assets/icon-192.png','./assets/icon-512.png','./assets/suedleder-icons.svg',
+  './Daten/rezepte.json','./Daten/gewichtsklassen.json','./Daten/stueckzahl_limits.json'
 ];
-self.addEventListener('install',e=>{
+self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
-  );
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
-
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
