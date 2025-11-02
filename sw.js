d72067dbde3b5fcb7995d3844771ddcd3ec284f9
oliverwunder20-sys <oliverwunder20@gmail.com>
2025-11-02 18:47:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
+const CACHE = 'aescher-v2';
+const ASSETS = [
+  './','./index.html','./styles.css','./app.js',
+  './manifest.json','./assets/icon-192.png','./assets/icon-512.png','./assets/suedleder-icons.svg'
+];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
 
